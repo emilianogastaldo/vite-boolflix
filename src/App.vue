174e518b5e75ed4a6fec068d85204b1747e7ae78
emilianogastaldo <emiliano0303@gmail.com>
2024-02-07 15:44:43 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-center">ciao</h1>
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
