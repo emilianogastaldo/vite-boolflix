cfb2a401b3aeadd1bb06c101d340b305804df6a3
emilianogastaldo <emiliano0303@gmail.com>
2024-02-07 18:21:45 +0100

    fixed search bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,10 +9,17 @@ export default {
 </script>
 
 <template>
-    <form @submit.prevent="$emit('search-movies', inputSearch)" id="searchBar">
-        <input type="text" placeholder="cerca" v-model.trim="inputSearch">
-        <button>Cerca</button>
-    </form>
+    <header>
+
+        <form @submit.prevent="$emit('search-movies', inputSearch)" id="searchBar">
+            <input type="text" placeholder="cerca" v-model.trim="inputSearch">
+            <button>Cerca</button>
+        </form>
+    </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    position: fixed;
+}
+</style>
