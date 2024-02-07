5f46ddf23b0c6e6c905c744ce8c1154ebe49edd6
emilianogastaldo <emiliano0303@gmail.com>
2024-02-07 16:18:36 +0100

    created AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+const endpoint = 'https://api.themoviedb.org/3/search';
+export default {
+  name: 'BoolFlix',
+  components: { AppHeader }
+}
 </script>
 
 <template>
+  <AppHeader />
   <h1 class="text-center">ciao</h1>
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data: () => ({
+        inputSearch: '',
+    })
+}
+</script>
+
+<template>
+    <form id="searchBar">
+        <input type="text" placeholder="cerca" v-model.trim="inputSearch">
+        <button>Cerca</button>
+    </form>
+</template>
+
+<style lang="scss" scoped></style>
