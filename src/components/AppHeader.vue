ba251bfbc9f6a22dbb6cc38d098d92af114a97e0
emilianogastaldo <emiliano0303@gmail.com>
2024-02-08 16:22:08 +0100

    added header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,7 +14,9 @@ export default {
 
 <template>
     <header>
-
+        <div class="logo">
+            <a href="#">LOGO SITO</a>
+        </div>
         <form @submit.prevent="$emit('search-movies', inputSearch)" id="searchBar">
             <input type="text" :placeholder="placeholder" v-model.trim="inputSearch">
             <button>{{ buttonLabel }}</button>
@@ -24,6 +26,18 @@ export default {
 
 <style lang="scss" scoped>
 header {
-    position: fixed;
+    padding: 0 4rem;
+    height: 100px;
+    position: sticky;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo a {
+        text-decoration: none;
+        color: white;
+        border: 1px solid white;
+        padding: 1rem 2rem;
+    }
 }
 </style>
