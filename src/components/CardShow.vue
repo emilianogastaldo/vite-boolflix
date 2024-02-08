735dff498ac92b75534cabf6780d3bca4d35deee
emilianogastaldo <emiliano0303@gmail.com>
2024-02-08 16:13:45 +0100

    aggiusto lo stile della card

--- src/components/CardShow.vue
+++ src/components/CardShow.vue
@@ -3,6 +3,7 @@ export default {
     name: 'CardShow',
     props: {
         show: Object,
+        maxVote: Number,
     },
     computed: {
         // Creo delle computed per i titoli e scrivere più veloce la lingua
@@ -16,7 +17,7 @@ export default {
             return this.show.original_language;
         },
 
-        // controllo se c'è l'immagine che mi interessa
+        // controllo se c'è l'immagine che mi interessa, mi devo scrivere quali immagini io abbia
         isImage() {
             const flags = ['en', 'it'];
             return flags.includes(this.lang);
@@ -24,15 +25,13 @@ export default {
         flagImg() {
             return new URL(`../assets/img/${this.lang}.png`, import.meta.url).href;
         },
+
         showImg() {
             return `https://image.tmdb.org/t/p/w342${this.show.poster_path}`
         },
         showVote() {
             return Math.floor(parseInt(this.show.vote_average) / 2);
         },
-        maxVote() {
-            return 5
-        }
     },
 }
 </script>
@@ -40,75 +39,56 @@ export default {
 <template>
     <!-- Card singola dello show -->
     <div class="show-card">
-        <figure>
-            <img v-if="show.poster_path" class="img-fluid" :src="showImg" :alt="title">
-            <div v-else class="noflag"> NO IMMAGINE </div>
-        </figure>
+        <div class="show-img">
+            <img v-if="show.poster_path" class="img-fluid d-block" :src="showImg" :alt="title">
+            <div v-else class="noflag"></div>
+        </div>
         <ul>
-            <li>Titolo: <strong>{{ title }}</strong> </li>
-            <li>Titolo originale: {{ originalTitle }}</li>
+            <li><strong>Titolo: </strong>{{ title }}</li>
+            <li><strong>Titolo originale: </strong>{{ originalTitle }}</li>
             <li>
-                <img v-if="isImage" class="img-fluid w-50" :src="flagImg" :alt="lang">
+                <img v-if="isImage" class="img-fluid w-25" :src="flagImg" :alt="lang">
                 <div v-else class="noflag">{{ lang }}</div>
             </li>
             <li>
-                <div class="d-flex">
+                <div class="d-flex gap-2">
                     Voto:
-                    <div v-for="i in maxVote">
+                    <div v-for="i in maxVote" :key="i">
                         <i v-if="i <= showVote" class="fas fa-star"></i>
                         <i v-else class="far fa-star"></i>
                     </div>
-                    {{ showVote }}
                 </div>
             </li>
-            <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quibusdam quas distinctio. Voluptatibus
-                nostrum animi necessitatibus dolorum vitae nam, itaque soluta consectetur modi, fugit doloremque, architecto
-                quisquam porro. Impedit, unde.
-                Sint odio maiores ratione tempora a deserunt iure soluta voluptatibus reiciendis debitis voluptatum optio
-                veniam magnam quae nemo, magni modi molestiae harum, quibusdam laudantium est rerum? Autem recusandae
-                obcaecati quos.
-                Provident iusto aut laborum libero debitis! Maiores rerum numquam aliquid fuga, quisquam vitae non beatae
-                natus, earum, iure voluptatem qui ut? Perferendis pariatur distinctio explicabo commodi soluta neque nulla
-                cum.
-                Porro veniam veritatis tenetur pariatur explicabo rerum aliquid dolorem dolore. At nemo, doloremque, cum
-                debitis ipsa quod necessitatibus esse illum blanditiis culpa quo accusantium itaque suscipit eveniet
-                inventore amet nisi?
-                Tempora dolorum consequuntur iusto. Tenetur quas est ratione aliquam? Blanditiis, repudiandae. Autem maxime
-                dolore sed, perspiciatis quia similique modi exercitationem nulla, maiores ullam consequatur corrupti
-                consequuntur error numquam vel voluptatum?
-                Et illo alias excepturi sint est architecto nihil odit! Quisquam alias, delectus eos nam totam
-                exercitationem quo voluptas modi et. Molestias aliquid et voluptatem consectetur quaerat vitae nulla, libero
-                at?
-                Ea, eaque facilis quae, dolorem soluta alias assumenda excepturi ducimus consequatur nam ullam architecto
-                reiciendis. Sunt animi, saepe ipsum voluptatem soluta praesentium fugiat ut quod eos, accusantium, sequi
-                quasi corporis.
-                Recusandae cupiditate amet a accusantium adipisci, facere debitis saepe aliquam repudiandae autem pariatur.
-                Sapiente aliquid rerum ducimus voluptates a velit labore incidunt voluptas minus iure. Quas necessitatibus
-                vitae quia blanditiis.
-                Impedit dolore ipsa ut voluptatem obcaecati, pariatur quisquam nobis qui beatae. Aut quaerat, obcaecati
-                delectus cumque vel beatae excepturi consectetur culpa ipsam sit ullam? Quas accusantium qui nemo numquam
-                assumenda.
-                Mollitia libero hic fugiat iste voluptate, ipsam expedita? Ab eligendi fugit aliquam, tempora unde enim
-                veniam explicabo exercitationem vitae assumenda delectus, molestiae suscipit sequi nesciunt quas minus
-                molestias ipsam temporibus.</li>
         </ul>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.noflag {
-    background-color: blueviolet;
+.show-card {
+    border: 1px solid white;
+    width: 300px;
 }
 
 .show-card ul {
-    height: 400px;
-    list-style-type: none;
+    height: 450px;
+    margin: 0;
     display: none;
+    list-style-type: none;
     overflow-y: auto;
-    background-color: lightcoral;
+    background-color: darkgray;
+    padding: 0 1rem;
+
+    li {
+        margin: 1rem 0;
+    }
 }
 
-.show-card:hover figure {
+.show-img {
+    margin: 0;
+    padding: 0;
+}
+
+.show-card:hover .show-img {
     display: none;
 }
 
